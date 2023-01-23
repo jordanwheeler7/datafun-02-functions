35f906ad9c13305ad0cec011a97f63bd4a2ddb93
jordanwheeler7 <122232837+jordanwheeler7@users.noreply.github.com>
2023-01-22 21:41:12 -0500

    First iteration

---
+++ user_mathout.py
@@ -0,0 +1,25 @@
+PS C:\Users\jorda\OneDrive\Desktop\Northwest Homework\DATA ANALYTICS FUNDAMENTALS\datafun-02-functions\datafun-02-functions> & C:/Users/jorda/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/jorda/OneDrive/Desktop/Northwest Homework/DATA ANALYTICS FUNDAMENTALS/datafun-02-functions/datafun-02-functions/user_math.py"
+
+
+Welcome! We will utilize this opportunity to record your last 3 sets of squats
+To begin, we will start with your last 3 sets of squats weight beginning with the most recent.
+
+Please enter the weight of your first set of squats: 315
+Please enter the weight of your second set of squats: 405
+Please enter the weight of your third set of squats: 225
+
+
+Please enter the number of reps for your first set: 8
+Please enter the number of reps for your second set: 3
+Please enter the number of reps for your third set: 10
+
+
+{945.0} was your total weight.
+{315.0} was your average weight.
+{28704375.0} was the product of your lifts.
+{225.0} was your lightest rep.
+{405.0} was your heaviest rep.
+{180.0} was the range of your lifts
+{486.0} is your new one rep max
+You are above average. Keep up the progress!
+Would you like to see how to improve your squats? (Y/N)
