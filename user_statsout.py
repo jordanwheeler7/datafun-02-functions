fba5c6ac5069b14dcc2f042140f221438ebf17ed
jordanwheeler7 <122232837+jordanwheeler7@users.noreply.github.com>
2023-01-22 22:23:16 -0500

    Intial_2

---
+++ user_statsout.py
@@ -0,0 +1,47 @@
+=============================================================
+
+Here's some weight data: [105, 129, 87, 86, 111, 111, 89, 81, 108, 92, 110, 100, 75, 105, 103, 109, 76, 119, 99, 91, 103, 129, 106, 101, 84, 111, 74, 87, 86, 103, 103, 106, 86, 111, 75, 87, 102, 121, 111, 88, 89, 101, 106, 95, 103, 107, 101, 81, 109, 104]
+
+Descriptive statistics include measures of central tendancy:
+   mean=99.12
+   median=102.50
+   mode=111.00
+
+Descriptive statistics include measures of spread:
+   var=179.25
+   stddev=13.39
+
+
+=============================================================
+
+See the following for weight and rep data:
+
+xtimes:[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+yvalues:[405, 385, 365, 315, 295, 275, 225, 225, 205, 185, 185, 135]
+
+These stats will show the correlation between the reps and weight
+
+   correlation between xreps and xreps = 1.00
+   correlation between xtimes and yvalues = -0.99
+
+
+
+Now we will see some of our data combined.
+
+x (reps):[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+y (pounds):[405, 385, 365, 315, 295, 275, 225, 225, 205, 185, 185, 135]
+
+We will calculate for the best fit straight line:
+
+   slope = -23.92
+   intercept = 422.12
+
+Now we will predict our future weight using the best fit line:
+
+   At 14, reps,
+   your predicted weight will be 87.
+
+
+PS C:\Users\jorda\OneDrive\Desktop\Northwest Homework\DATA ANALYTICS FUNDAMENTALS\datafun-02-functions\datafun-02-functions>
