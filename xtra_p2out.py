18b95b8bc5baff4da7ee13e55b45c16044fbe1b9
jordanwheeler7 <122232837+jordanwheeler7@users.noreply.github.com>
2023-01-24 10:04:43 -0500

    Extra Turn It

---
+++ xtra_p2out.py
@@ -0,0 +1,89 @@
+"""
+Name: Jordan Wheeler
+Date: 24 Jan 2023
+xtra output (fixing mistakes.)
+
+PS C:\Users\jorda\OneDrive\Desktop\Northwest Homework\DATA ANALYTICS FUNDAMENTALS\datafun-02-functions\datafun-02-functions> & C:/Users/jorda/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/jorda/OneDrive/Desktop/Northwest Homework/DATA ANALYTICS FUNDAMENTALS/datafun-02-functions/datafun-02-functions/xtra_p2.py"
+===========================================================
+Running doctest.testmod() function to unit test our code
+===========================================================
+
+
+===========================================================
+If you don't see any results, all tests passed.
+===========================================================
+Run with the -v flag to show results regardless.
+Hit up arrow (to get last command) and add space -v
+===========================================================
+
+Read error messages carefully.
+Errors tell which line number has the error
+and what the issue is. Scroll up in the terminal to see.
+Fix issues one at a time until you get the behavior
+Described in the docstring.
+PS C:\Users\jorda\OneDrive\Desktop\Northwest Homework\DATA ANALYTICS FUNDAMENTALS\datafun-02-functions\datafun-02-functions> & C:/Users/jorda/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/jorda/OneDrive/Desktop/Northwest Homework/DATA ANALYTICS FUNDAMENTALS/datafun-02-functions/datafun-02-functions/xtra_p2.py" -v 
+===========================================================
+Running doctest.testmod() function to unit test our code
+===========================================================
+
+Trying:
+    add_two(1,2)
+Expecting:
+    3
+ok
+Trying:
+    add_two("hello"," world")
+Expecting:
+    'hello world'
+ok
+Trying:
+    add_triangle_list( [3,4,5] )
+Expecting:
+    12
+ok
+Trying:
+    add_any(1,1,1,1,1,1,1,1)
+Expecting:
+    8
+ok
+Trying:
+    add_any_with_keywords(a=1,b=2)
+Expecting:
+    3
+ok
+Trying:
+    convert_ctof(0)
+Expecting:
+    32.0
+ok
+Trying:
+    convert_ctof(100)
+Expecting:
+    212.0
+ok
+5 items had no tests:
+    __main__.add_any
+    __main__.add_any_with_keywords
+    __main__.add_triangle_list
+    __main__.add_two
+    __main__.convert_ctof
+1 items passed all tests:
+   7 tests in __main__
+7 tests in 6 items.
+7 passed and 0 failed.
+Test passed.
+
+===========================================================
+If you don't see any results, all tests passed.
+===========================================================
+Run with the -v flag to show results regardless.
+Hit up arrow (to get last command) and add space -v
+===========================================================
+
+Read error messages carefully.
+Errors tell which line number has the error
+and what the issue is. Scroll up in the terminal to see.
+Fix issues one at a time until you get the behavior
+Described in the docstring.
+PS C:\Users\jorda\OneDrive\Desktop\Northwest Homework\DATA ANALYTICS FUNDAMENTALS\datafun-02-functions\datafun-02-functions>
+"""
